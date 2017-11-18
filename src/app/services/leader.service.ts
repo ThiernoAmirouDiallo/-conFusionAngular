8fedfa9ac925bc564195b6f2cf6c4859dceb6dad
TAmirouD <ThiernoAmirou.DIALLO2@orange-sonatel.com>
2017-11-18 01:22:49 +0000

    Promise Part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { resolve } from 'url';
 
 @Injectable()
 export class LeaderService {
@@ -8,14 +9,26 @@ export class LeaderService {
   constructor() { }
 
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    //return Promise.resolve(LEADERS);
+    return new Promise(resolve =>
+    {
+      setTimeout(()=>resolve(LEADERS),2000)
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    //return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve =>
+      {
+        setTimeout(()=>resolve(LEADERS.filter((leader) => (leader.id === id))[0]),2000)
+      });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+    //return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+    return new Promise(resolve =>
+      {
+        setTimeout(()=>resolve(LEADERS.filter((leader) => leader.featured)[0]),2000)
+      });
   }
 }
